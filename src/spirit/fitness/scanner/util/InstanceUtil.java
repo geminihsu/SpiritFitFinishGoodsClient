483a0f58d7eeb59c88c83a0b665e4786fdfe303a
SPIRITLOCAL\geminih <gemini612gemini@gmail.com>
2018-06-18 11:39:28 -0500

    Added save shipping report to daily report function.

--- src/spirit/fitness/scanner/util/InstanceUtil.java
+++ src/spirit/fitness/scanner/util/InstanceUtil.java
@@ -5,6 +5,7 @@ import spirit.fitness.scanner.delegate.ItemPannelBaseViewDelegate;
 import spirit.fitness.scanner.receving.ContainerPannel;
 
 import spirit.fitness.scanner.report.DailyReport;
+import spirit.fitness.scanner.report.DailyShippingReport;
 import spirit.fitness.scanner.report.ModelZone2Report;
 import spirit.fitness.scanner.search.QueryPannel;
 import spirit.fitness.scanner.shipping.ShippingConfirm;
@@ -15,7 +16,7 @@ public class InstanceUtil {
 	public static boolean isExits() 
 	{
 	
-		if(AppMenu.isExistInstance || ItemPannelBaseViewDelegate.isExit() || QueryPannel.isExit() || ContainerPannel.isExit() || ShippingConfirm.isExit() || DailyReport.isExit() || ShippingPicking.isExit() || ModelZone2Report.isExit())
+		if(AppMenu.isExistInstance || ItemPannelBaseViewDelegate.isExit() || QueryPannel.isExit() || ContainerPannel.isExit() || ShippingConfirm.isExit() || DailyReport.isExit() || ShippingPicking.isExit() || ModelZone2Report.isExit()||DailyShippingReport.isExit())
 			return true;
 		return false;
 	}
